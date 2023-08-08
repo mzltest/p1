a28125a7037bd70179b23d49174d7c43a0bb567a
mzltest <52124882+mzltest@users.noreply.github.com>
2023-08-09 00:48:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,4 +43,4 @@ install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 # Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+${DIR_RUNTIME}/v2ray --config=${DIR_CONFIG}/config.pb
